5227d06ea9cc361ff367b84db4b6b469a6458fb8
logitha <loga848.456@gmail.com>
2016-07-09 11:12:03 +0530

    Create aqw.c

---
+++ aqw.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5],a1,b=0;
+clrscr();
+for(a1=0;a1<5;a1++)
+{
+scanf("%d",&a[a1]);
+}
+if(b==a[4])
+{
+printf("False");
+}
+else
+{
+printf("True");
+}
+getch();
+}
